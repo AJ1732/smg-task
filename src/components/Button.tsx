54b821d62d29d49c507106380f7ccba8a9a3a13e
AJ1732 <ejemeniboi@gmail.com>
2024-06-26 22:55:12 +0100

    chore: create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+const Button = ({ children }: Props) => {
+  return (
+    <div className="rounded-lg bg-black-0 px-6 py-3 font-semibold text-grey-50">
+      {children}
+    </div>
+  );
+};
+export default Button;

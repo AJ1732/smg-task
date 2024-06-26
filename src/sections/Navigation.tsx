03eb13c4f8cd37b01e8a18583812c85dad07b1e3
AJ1732 <ejemeniboi@gmail.com>
2024-06-26 22:05:27 +0100

    chore: layout navigation

--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -1,4 +1,23 @@
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <header className="flex-between size-full">
+      {/* LOGO */}
+      <h1>Hand</h1>
+
+      {/* NAV LINKS */}
+      <nav>
+        <ul className="flex-between gap-4 [&>li]:p-2">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+
+      {/* NAV BUTTON */}
+      <button>Start a project</button>
+    </header>
+  );
 };
 export default Navigation;

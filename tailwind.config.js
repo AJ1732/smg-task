fda5158261671407f0442d7bfda7742131935e45
AJ1732 <ejemeniboi@gmail.com>
2024-06-26 22:46:57 +0100

    fix: import correct extension

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          0o0: "#FDCA09",
+          100: "#FEC00A",
+        },
+        secondary: {
+          100: "#C82090",
+        },
+        grey: {
+          50: "#EFF0F1",
+          500: "#5A6675",
+          600: "#525D6A",
+        },
+        black: {
+          0o0: "#0B0B0B",
+          100: "#101010",
+        },
+      }
+    },
+  },
+  plugins: [],
+};

14bbda7dca347832d776b82fa25056ce291fd960
AJ1732 <ejemeniboi@gmail.com>
2024-06-26 21:51:09 +0100

    chore: create structure for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import Footer from "./sections/Footer";
+import Main from "./sections/Main";
+import Navigation from "./sections/Navigation";
 
+function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  )
+    <div className="content-grid all *:border *:border-neutral-950">
+      <Navigation />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;

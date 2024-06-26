da3257c96f46cd29bafb6613601edadafceef241
AJ1732 <ejemeniboi@gmail.com>
2024-06-26 22:25:20 +0100

    chore: add custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      primary: {
+        0o0: "#FDCA09",
+        100: "#FEC00A",
+      },
+      secondary: {
+        100: "#C82090",
+      },
+      grey: {
+        50: "#EFF0F1",
+        500: "#5A6675",
+        600: "#525D6A",
+      },
+      black: {
+        0o0: "#0B0B0B",
+        100: "#101010",
+      },
+    },
   },
   plugins: [],
-}
-
+};

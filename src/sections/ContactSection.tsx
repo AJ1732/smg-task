c030c293ef0660454b0020c0e64e45fe88c99570
AJ1732 <ejemeniboi@gmail.com>
2024-06-27 15:41:13 +0100

    chore: adjust image sizing

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -5,10 +5,10 @@ const ContactSection = () => {
   return (
     <section>
       <div className="flex flex-col-reverse items-center lg:flex-row lg:items-start justify-between gap-20 lg:gap-8 py-[12.75rem]">
-        <figure className="flex-center h-[55.6875rem] w-[38.875rem] bg-[#FEF1E0]">
+        <figure className="flex-center size-full p-4 pt-10 min-h-[55.6875rem] max-w-[38.875rem] bg-[#FEF1E0]">
           <img
             src={contact}
-            className="max-w-[23.625rem]"
+            className="max-w-[23.625rem] max-md:-mr-10 w-full"
             alt="Two guys standing and smiling warmly, waiting for you to contact them"
           />
         </figure>

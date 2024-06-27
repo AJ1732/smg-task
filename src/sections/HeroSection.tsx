cabe4bbe6f32f0fc92ac880a1360488752f3b4b1
AJ1732 <ejemeniboi@gmail.com>
2024-06-27 15:57:22 +0100

    chore: make hero page responsive

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -11,16 +11,16 @@ const HeroSection = () => {
   return (
     <section className="full-width content-grid bg-primary-0">
       <div className="content relative flex flex-col items-center justify-between pt-[10.25rem] xl:min-h-[56.56rem]">
-        <h2 className="text-center z-10 text-[6.25rem] font-bold leading-[6.875rem]">
+        <h2 className="z-10 text-center text-5xl font-bold leading-[4rem] md:text-6xl md:leading-[5rem] lg:text-7xl lg:leading-[6.875rem] xl:text-[6.25rem]">
           Let’s create something <br /> great together.
         </h2>
 
-        <figure className="absolute inset-0 flex justify-center items-start size-full ">
-          <img src={blob_2} className="w-[32.06275rem] -mr-44" />
-          <img src={blob_1} className="w-[80.4225rem] -mt-10" />
+        <figure className="absolute inset-0 flex size-full flex-col items-start justify-center lg:flex-row">
+          <img src={blob_2} className="w-[32.06275rem] lg:-mr-44" />
+          <img src={blob_1} className="w-[80.4225rem] lg:-mt-10" />
         </figure>
 
-        <figure className="flex z-10 items-center justify-center [&>img]:max-w-[15.2rem]">
+        <figure className="z-10 flex items-center justify-center [&>img]:w-full [&>img]:max-w-[15.2rem]">
           <img src={hero_1} alt="hero image 1" />
           <img src={hero_2} alt="hero image 1" />
           <img src={hero_3} alt="hero image 1" />

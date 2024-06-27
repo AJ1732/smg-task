d45e9f68065001a406888c90ab588320426ab228
AJ1732 <ejemeniboi@gmail.com>
2024-06-27 16:08:59 +0100

    chore: add curosor pointer

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Button = ({ children }: Props) => {
   return (
-    <div className="rounded-lg bg-black-0 px-6 py-3 leading-6 font-semibold text-grey-50">
+    <div className="rounded-lg cursor-pointer bg-black-0 px-6 py-3 leading-6 font-semibold text-grey-50">
       {children}
     </div>
   );

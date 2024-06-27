56ae4c71b8aacec21f23e7fea4ae6aa1624c1858
AJ1732 <ejemeniboi@gmail.com>
2024-06-27 01:41:07 +0100

    chore: building figure part

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -1,4 +1,21 @@
+import ContactForm from "../components/form/ContactForm";
+import contact from "../assets/img/contact.png";
+
 const ContactSection = () => {
-  return <div>ContactSection</div>;
+  return (
+    <section>
+      <div className="flex-between py-[12.75rem]">
+        <figure className="flex-center h-[55.6875rem] w-[38.875rem] bg-[#FEF1E0]">
+          <img
+            src={contact}
+            className="max-w-[23.625rem]"
+            alt="Two guys standing and smiling warmly, waiting for you to contact them"
+          />
+        </figure>
+
+        <ContactForm />
+      </div>
+    </section>
+  );
 };
 export default ContactSection;

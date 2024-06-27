4cfe38ae1425a21ae2f85288a1176c08125097cb
AJ1732 <ejemeniboi@gmail.com>
2024-06-27 02:10:03 +0100

    chore: build form element

--- src/components/form/ContactForm.tsx
+++ src/components/form/ContactForm.tsx
@@ -1,4 +1,36 @@
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  console.log("Submitted");
+};
+
 const ContactForm = () => {
-  return <form>ContactForm</form>;
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="size-full max-w-[46.9375rem] text-black-0"
+    >
+      <fieldset className="flex-end flex-col gap-7">
+        <legend className="mb-12 ml-auto">
+          <h3 className="text-right text-[2.75rem] font-bold">
+            We'd love to hear <br />
+            from you
+          </h3>
+        </legend>
+
+        <input type="text" name="name" id="name" placeholder="Name" />
+        <input type="email" name="email" id="email" placeholder="Email" />
+        <input type="url" name="url" id="url" placeholder="Website Url" />
+        <textarea
+          name="project"
+          id="project"
+          placeholder="Project Details"
+        ></textarea>
+
+        <button className="w-full rounded-lg bg-black-100 py-9 text-center text-3xl font-medium text-white">
+          Send Project
+        </button>
+      </fieldset>
+    </form>
+  );
 };
 export default ContactForm;
